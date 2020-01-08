131a7999481379a089b9c6ec1d644e2f96c2a614
jiaeyan <svenyan234@gmail.com>
2020-01-07 23:51:37 -0800

    finished feature selection

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -1,5 +1,6 @@
 from math import log2
 from collections import Counter
+from collections import defaultdict
 import numpy as np
 
 class DecisionTree:
@@ -21,20 +22,27 @@ class DecisionTree:
     def cond_entropy(self, X, Y, feature_id):
         """
         H(D|F)
+        Collect the y list under each value type of feature_id. 
         """
-        features = X[:, feature_id]
-        F_count = Counter(features)
-        
+        F_Y_dict = defaultdict(list)
+        for x, y in zip(X, Y):
+            F_Y_dict[x[feature_id]].append(y)
+        return np.sum((len(y_list)/len(Y)) * self.data_entropy(y_list) for y_list in F_Y_dict.values())
 
-    def info_gain(self, entroy, cond_entropy):
-        pass
+    def info_gain(self, data_entropy, cond_entropy):
+        return data_entropy - cond_entropy
 
     def train(self, X, Y):
         pass
 
     def feature_selection(self, X, Y):
-        entropy = self.data_entropy(Y)
-        cond_entropy = self.cond_entropy(X, Y)
+        d_ent = self.data_entropy(Y)
+        info_gain_list = []
+        for feature_id in range(len(X[0])):
+            cond_ent = self.cond_entropy(X, Y, feature_id)
+            info_gain_list.append((self.info_gain(d_ent, cond_ent), feature_id))
+        return max(info_gain_list)
+        
 
     def grow_tree(self, X, Y):
         pass

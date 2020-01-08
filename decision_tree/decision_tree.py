8df989d881e8211fe2ab9b6f6e54c0b10a68e132
jiaeyan <svenyan234@gmail.com>
2020-01-08 09:47:33 -0800

    implementing train function

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -8,9 +8,10 @@ class DecisionTree:
     class DTNode:
         def __init__(self):
             self.label = None
+            self.children = {}
 
-    def __init__(self):
-        pass
+    def __init__(self, info_gain_threshold=0.1):
+        self.info_gain_threshold = info_gain_threshold
 
     def data_entropy(self, Y):
         """
@@ -33,7 +34,21 @@ class DecisionTree:
         return data_entropy - cond_entropy
 
     def train(self, X, Y):
-        pass
+        if len(set(Y)) == 1:
+            pass
+        if len(X[0]) == 1:
+            pass
+        info_gain, best_feature = self.feature_selection(X, Y)
+
+        if info_gain <= self.info_gain_threshold:
+            pass
+
+        best_f_value_set = set(X[:, best_feature])
+
+        for f in best_f_value_set:
+            sub_X, sub_Y = zip(*[(np.delete(x, best_feature), y) for x, y in zip(X, Y) if x[best_feature] == f])
+            # train(np.array(sub_X), np.array(sub_Y))
+
 
     def feature_selection(self, X, Y):
         d_ent = self.data_entropy(Y)
@@ -52,3 +67,18 @@ class DecisionTree:
 
     def predict(self, X, Y):
         pass
+
+if __name__ == '__main__':
+    y = np.array([['a', 'b', 'c'], ['c', 'a', 'c'], ['v', 'c', 'a'], ['d', 's', 'c']])
+    # mask = np.isin(y, ['c'])
+    # print(mask)
+    # print(y[mask])
+    # print(np.select(np.array([y[-1] =='c']*len(y)), y))
+    # y_np = np.array(y)
+    # print(type(y_np))
+    # a = np.array([np.delete(x, 1) for x in y])
+    # print(a[:, -1])
+    a = [(1,'a'), (2, 'b'), (3, 'c')]
+    b, c = list(zip(*a))
+    print(type(b))
+    print(c)

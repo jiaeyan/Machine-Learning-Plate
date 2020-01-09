1af5975ce0197911ac8ddd74952429eb49885c0d
jiaeyan <svenyan234@gmail.com>
2020-01-08 23:45:53 -0800

    organize the codes in dt

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -6,23 +6,63 @@ from sklearn.metrics import classification_report
 
 np.random.seed(42)
 
+class Node:
+
+    def __init__(self, is_terminal=False, label=None, feature_id=None):
+        # properties of internal/feature nodes
+        self.feature_id = feature_id
+        self.children = {}
+
+        # properties of terminal/label nodes
+        self.is_terminal = is_terminal
+        self.label = label
+
 class DecisionTree:
-
-    class Node:
-        def __init__(self, is_terminal=False, label=None, feature_id=None):
-            # properties of internal/feature nodes
-            self.feature_id = feature_id
-            self.children = {}
-
-            # properties of terminal/label nodes
-            self.is_terminal = is_terminal
-            self.label = label
-
 
     def __init__(self, info_gain_threshold=0.1, mode='id3'):
         self.info_gain_threshold = info_gain_threshold
         self.mode = mode
         self.tree = None
+    
+    def train(self, X, Y):
+        self.tree = self.generate_tree(X, Y)
+        # prune the tree
+    
+    def generate_tree(self, X, Y):
+
+        # if all labels are the same, make a terminal node
+        if len(set(Y)) == 1:
+            return Node(is_terminal=True, label=Y[0])
+
+        # if no selectable feature left, make a terminal node
+        if len(X[0]) == 0:
+            return Node(is_terminal=True, label=Counter(Y).most_common()[0][0])
+
+        # Step 1: select the feature with the highest info gain
+        info_gain, best_feature_id = self.select_feature(X, Y)
+
+        # if info gain is too small, make a terminal node
+        if info_gain <= self.info_gain_threshold:
+            return Node(is_terminal=True, label=Counter(Y).most_common()[0][0])
+        
+        # Step 2: grow the tree
+        node = self.expand_tree(X, Y, best_feature_id)
+
+        return node
+    
+    def select_feature(self, X, Y):
+        d_ent = self.data_entropy(Y)
+        info_gain_list = []
+
+        for feature_id in range(len(X[0])):
+            cond_ent = self.cond_entropy(X, Y, feature_id)
+            if self.mode == 'id3':
+                info_gain_list.append((self.info_gain(d_ent, cond_ent), feature_id))
+            elif self.mode == 'c4.5':
+                f_d_ent = self.f_data_entropy(X, Y, feature_id)
+                info_gain_list.append((self.info_gain_ratio(d_ent, cond_ent, f_d_ent), feature_id))
+
+        return max(info_gain_list)
 
     def data_entropy(self, Y):
         Y_count = Counter(Y)
@@ -45,56 +85,12 @@ class DecisionTree:
     
     def info_gain_ratio(self, data_entropy, cond_entropy, f_data_entropy):
         return (data_entropy - cond_entropy) / f_data_entropy
-    
-    def train(self, X, Y):
-        self.tree = self.generate_tree(X, Y)
-
-        # prune the tree
-
-    def generate_tree(self, X, Y):
-
-        # if all labels are the same, make a terminal node
-        if len(set(Y)) == 1:
-            return self.Node(is_terminal=True, label=Y[0])
-
-        # if no selectable feature left, make a terminal node
-        if len(X[0]) == 0:
-            return self.Node(is_terminal=True, label=Counter(Y).most_common()[0][0])
-
-        # Step 1: select the feature with the highest info gain
-        info_gain, best_feature_id = self.select_feature(X, Y)
-
-        # if info gain is too small, make a terminal node
-        if info_gain <= self.info_gain_threshold:
-            return self.Node(is_terminal=True, label=Counter(Y).most_common()[0][0])
         
-        # Step 2: grow the tree
-        node = self.expand_tree(X, Y, best_feature_id)
-
-        return node
-
-
-    def select_feature(self, X, Y):
-        d_ent = self.data_entropy(Y)
-        info_gain_list = []
-
-        for feature_id in range(len(X[0])):
-            cond_ent = self.cond_entropy(X, Y, feature_id)
-
-            if self.mode == 'id3':
-                info_gain_list.append((self.info_gain(d_ent, cond_ent), feature_id))
-            elif self.mode == 'c4.5':
-                f_d_ent = self.f_data_entropy(X, Y, feature_id)
-                info_gain_list.append((self.info_gain_ratio(d_ent, cond_ent, f_d_ent), feature_id))
-
-        return max(info_gain_list)
-        
-
     def expand_tree(self, X, Y, feature_id):
-        node = self.Node(feature_id=feature_id)
-        best_f_value_set = set(X[:, feature_id])
+        node = Node(feature_id=feature_id)
+        f_val_set = set(X[:, feature_id])
 
-        for f in best_f_value_set:
+        for f in f_val_set:
             sub_X, sub_Y = zip(*[(np.delete(x, feature_id), y) for x, y in zip(X, Y) if x[feature_id] == f])
             sub_tree = self.generate_tree(np.array(sub_X), np.array(sub_Y))
             node.children[f] = sub_tree
